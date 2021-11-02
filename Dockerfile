579a530b501cddad1a34b7fcc2a1c202ca26eaf4
jnhck <s2jaheuc@uni-trier.de>
2021-11-02 15:12:35 +0100

    ensuring compatiblity with frontend

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
 FROM node:14-slim
 
-WORKDIR /src
+# Create app directory
+WORKDIR /app
 
-ADD package.json package-lock.json ./
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+# where available (npm@5+)
+COPY package*.json .
 
 RUN npm install
 
-ADD . . 
+# Bundle app source
+COPY . .
 
+ENTRYPOINT [ "node", "app" ]
--- app.js
+++ app.js
@@ -2,17 +2,15 @@ const axios = require('axios');
 const express = require("express");
 const app = express();
 
-const port = 3000;
+const port = 4001;
 
 app.listen(port, () => {
-  console.log("Listening at http://localhost:" + port);
+  console.log("Weather microservice running!");
 });
 
-
-app.get("/api/city/w=:city(*)", function(req, res) { 
-  const city = req.params.city; // the city that is passed via the URL
+app.get("/api/weather/", function(req, res) { 
+  const city = req.query.c; // the city that is passed via the URL
   const url  = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2e15c965ff28cc6a856c04b3489f9d68`;
-
 
     axios
       .get(url)
@@ -27,12 +25,11 @@ app.get("/api/city/w=:city(*)", function(req, res) {
         const temperatureF = (temperatureK * 9) / 5 - 459.67;
         const temperatureC = temperatureK - 273.15;
 
-
         const weatherDisplay = `Right now, in \
             ${cityName}, ${countryName} the current temperature is \
-            ${temperatureC.toFixed(1)}ºC \
-            (${temperatureF.toFixed(1)}ºF), with ${humidity}% humidity, \
-            Conditions: ${response.data.weather[0].description} `.replace(/\s+/g, " ");
+            ${temperatureC.toFixed(1)} ºC \
+            (${temperatureF.toFixed(1)} ºF), with ${humidity}% humidity, \
+            conditions: ${response.data.weather[0].description} `.replace(/\s+/g, " ");
 
         res.send(weatherDisplay)
       })
